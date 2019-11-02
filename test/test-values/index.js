9ed6daba8fb83543512ab658f468e5525376377f
weaponsforge <forgeweapons@gmail.com>
2019-11-03 04:35:45 +0800

    Updated README.

--- test/test-values/index.js
+++ test/test-values/index.js
@@ -3,11 +3,13 @@ const mixedFraction = require('./mixedfraction-test')
 const parseStringInt = require('./parsestringint-test')
 const sumByFactors = require('./sumbyfactors-test')
 const smallestTest = require('./smallest-test')
+const topThreeWords = require('./topthreewords-test')
 
 module.exports = {
   formatDuration,
   mixedFraction,
   parseStringInt,
   sumByFactors,
-  smallestTest
+  smallestTest,
+  topThreeWords
 }
